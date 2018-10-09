cca5232179358d7423605db59af41eaa69e538bf
giulio <giulioscuro@gmail.com>
2018-10-09 23:54:47 +0200

    database scripts

--- src/main/java/it/giulioscuro/ricettelight/controller/WebController.java
+++ src/main/java/it/giulioscuro/ricettelight/controller/WebController.java
@@ -47,7 +47,7 @@ public class WebController {
 		 String[] arraySearch =  searchString.split(",");
 		   List<Ricetta> ricette = service.search(arraySearch);
 	        model.addAttribute("ricette", ricette);
-	        return "ricette";
+	        return "ricete";
 	    }
 	  
 	  @RequestMapping("/web/ricette2")
